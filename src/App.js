8ec822a8d138f7d8a37f614b2383c632e6f08770
chochchill <chaithanya1359@gmail.com>
2021-08-08 18:17:57 +0530

    takes imput word changes the main title to that word takes input language , displays meanings, synonyms, examples, audio(phenotics) if present

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container } from "@material-ui/core";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Definitions from "./components/definitions/Definitions";
 function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+      // console.log(data);
+      setMeanings(data.data);
+    } catch (err) {}
+  };
+  console.log(meanings);
+  useEffect(() => {
+    dictionaryApi();
+  }, [category, word]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <Header
+          word={word}
+          setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+        />
+        {meanings && (<Definitions
+          word={word}
+          category={category}
+          meanings={meanings}
+        />)}
+      </Container>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Header.css";
+import TextField from "@material-ui/core/TextField";
+import {
+  createTheme,
+  MenuItem,
+  ThemeProvider,
+} from "@material-ui/core";
+import categories from "../../data/category";
+const Header = (props) => {
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+  const handleChange = (language) =>{
+    props.setCategory(language);
+    props.setWord("")
+  }
+  
+  return (
+    <div className="header">
+      <span className="title">{props.word ? props.word : "Word Hunt"}</span>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" className="search" label="Standard" 
+            onChange={(e) => props.setWord(e.target.value)}
+            value={props.word} />
+          <TextField
+            select
+            className="select"
+            id="standard-basic"
+            label="Language"
+            value={props.category}
+            onChange={(e) => handleChange(e.target.value)}
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.label} value={option.label}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
